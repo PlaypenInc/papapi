40eea16c3e3c7ddb1cef81f4d5a46da8eed86521
Gavin Dunne <g@veen.ca>
2012-01-18 00:48:23 -0800

    affiliate updates fields on save, and added method to add affiliate to commission group.

--- lib/papapi/affiliate.rb
+++ lib/papapi/affiliate.rb
@@ -1,48 +1,67 @@
 module Papapi
-  class Affiliate < Hashie::Dash
+  class Affiliate < Hashie::Trash
     property :username
-    property :password
     property :firstname
     property :lastname
-    property :url
-    property :company
-    property :street
-    property :city
-    property :state
-    property :country
-    property :zip
-    property :phone
-    property :refid
     property :parentuserid
+    property :refid
+    property :userid
+    property :rpassword,  :from => :password
+    property :data1,      :from => :url
+    property :data2,      :from => :company
+    property :data3,      :from => :street
+    property :data4,      :from => :city
+    property :data5,      :from => :state
+    property :data6,      :from => :country
+    property :data7,      :from => :zip
+    property :data8,      :from => :phone
 
     def save!
+      update_properties(
+        PostRequest.new(
+          :connection  => Papapi.connection,
+          :class_name  => 'Pap_Signup_AffiliateForm',
+          :method_name => 'add',
+          :arguments   => to_hash
+        ).response.fields
+      )
+      true
+    end
+
+    # update properties ignoring those that don't exist
+    def update_properties (new_props)
+      new_props.each do |key,value|
+        send("#{key}=", value) if respond_to?("#{key}=")
+      end
+    end
+
+    def add_to_commission_group (campaing_id, commission_group_id, opt = {})
+      raise 'Affiliate has no userid, make sure it is set first' if userid.nil?
+      
+      # Default options
+      opt[:status] ||= 'A'
+      opt[:note]   ||= ''
+      
       PostRequest.new(
         :connection  => Papapi.connection,
-        :class_name  => 'Pap_Signup_AffiliateForm',
+        :class_name  => 'Pap_Features_Common_AffiliateGroupForm',
         :method_name => 'add',
         :arguments   => {
-          'username'       => username,
-          'rpassword'      => password,
-          'firstname'      => firstname,
-          'lastname'       => lastname,
-          'parentuserid'   => parentuserid,
-          'data1'          => url,
-          'data2'          => company,
-          'data3'          => street,
-          'data4'          => city,
-          'data5'          => state,
-          'data6'          => country,
-          'data7'          => zip,
-          'data8'          => phone,
-          'refid'          => refid,
-          'agreeWithTerms' => 'Y'
+          'Id'                => '',
+          'userid'            => userid,
+          'rstatus'           => opt[:status],
+          'note'              => opt[:note],
+          'commissiongroupid' => commission_group_id,
+          'campaignid'        => campaing_id
         }
       ).response
     end
 
     class << self
       def create! (params)
-        new(params).save!
+        a = new(params)
+        a.save!
+        a
       end
     end
   end
