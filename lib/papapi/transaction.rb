58ddf5d58bd4360e964e7e7966ff4d13c12cbe14
Gavin Dunne <g@veen.ca>
2012-01-26 19:32:25 -0800

    added data fields to transactions

--- lib/papapi/transaction.rb
+++ lib/papapi/transaction.rb
@@ -10,6 +10,10 @@ module Papapi
     property :dateinserted
     property :orderid
     property :productid
+    property :data1
+    property :data2
+    property :data3
+    property :data4
     property :rstatus,      :from => :status, :default => 'P'   # (A)pproved (P)ending
     property :rtype,        :from => :type,   :default => 'S'   # (S)ale
     property :multiTier,    :default => 'Y'   # Yes
